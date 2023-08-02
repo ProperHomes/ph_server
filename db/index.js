01c9b42fc44300d8e011d2c6f7189de7e18a4962
factsfinder <phanindrasrikar@gmail.com>
2023-08-03 01:09:04 +0530

    fix db connection

--- db/index.js
+++ db/index.js
@@ -3,9 +3,9 @@ const { Pool } = require("pg");
 
 const pgPool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = { pgPool };
